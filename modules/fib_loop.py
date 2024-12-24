e25909601cdc28bb0e8e61fbd02d69259aef5b8e
GENADEVICH <116941467+GENADEVICH@users.noreply.github.com>
2024-12-24 22:38:23 +0300

    v1.2

--- modules/fib_loop.py
+++ modules/fib_loop.py
@@ -11,7 +11,8 @@ def fib(n):
 if __name__ == "__main__":
     test_values = [10, 15, 20, 25, 35]
     for n in test_values:
-        start = time.time()
+        start = time.perf_counter()
         result = fib(n)
-        end = time.time()
-        print(f"fib({n}) = {result}, Time: {round((end - start) * 1000, 2)}ms")
+        end = time.perf_counter()
+        elapsed_time = (end - start) * 1000
+        print(f"fib({n}) = {result}, Time: {elapsed_time:.6f}ms")
